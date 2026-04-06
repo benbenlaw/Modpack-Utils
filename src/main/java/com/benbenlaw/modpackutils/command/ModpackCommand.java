32d511d006515a52b57588f78b6d6e3434f7566e
benbenlaw <benlawrenceben@hotmail.co.uk>
2026-04-06 14:51:26 +0100

    1.2.0

--- src/main/java/com/benbenlaw/modpackutils/command/ModpackCommand.java
+++ src/main/java/com/benbenlaw/modpackutils/command/ModpackCommand.java
@@ -4,11 +4,15 @@ import com.benbenlaw.modpackutils.config.MUConfig;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
+import dev.wuffs.bcc.BetterCompatibilityChecker;
+import dev.wuffs.bcc.Config;
+import dev.wuffs.bcc.data.BetterStatus;
 import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
+import net.neoforged.fml.ModList;
 
 public class ModpackCommand {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
@@ -18,8 +22,20 @@ public class ModpackCommand {
     private static int execute(CommandContext<CommandSourceStack> command) {
         if (command.getSource().getEntity() instanceof Player player) {
 
+            String modpackName = MUConfig.modpackName.get();
+            String modpackVersion = MUConfig.modpackVersion.get();
+
+            if (ModList.get().isLoaded("bcc")) {
+                if (MUConfig.modpackName.get().equals("USE_BCC")) {
+                    modpackName = Config.data().modpackName().value();
+                }
+                if (MUConfig.modpackVersion.get().equals("USE_BCC")) {
+                    modpackVersion = Config.data().modpackVersion().value();
+                }
+            }
+
             if (!MUConfig.modpackName.get().isEmpty() && !MUConfig.modpackVersion.get().isEmpty()) {
-                player.sendSystemMessage(Component.translatable("chat.modpackutils.modpack", MUConfig.modpackName.get(), MUConfig.modpackVersion.get())
+                player.sendSystemMessage(Component.translatable("chat.modpackutils.modpack", modpackName, modpackVersion)
                         .withStyle(ChatFormatting.BLUE));
             } else {
                 player.sendSystemMessage(Component.translatable("chat.modpackutils.modpack_not_set")
